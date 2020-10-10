a06155039467e393fe0a693b592d44346689ed40
khermalik <khermalik@gmail.com>
2020-10-09 18:33:51 -0700

    Set up file structure and dependencies, Connected to server and Root Route successfully

---
+++ server.js
@@ -0,0 +1,33 @@
+
+// connection to server
+const express = require('express');
+const app = express();
+
+// 3rd party modules
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+require('dotenv').config();
+const PORT = process.env.PORT || 4000;
+
+// set view engine
+app.set('view engine', 'ejs');
+
+// CONTROLLERS
+
+// Middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use(methodOverride('_method'));
+
+// ------------------------------------------------------ Routes
+
+// Root route (landing page)
+app.get('/', (req, res) => {
+  res.render('./landing');
+});
+
+
+//Listener
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
